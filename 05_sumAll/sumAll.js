6ab467831707dc6e00a531e1b1ffa19c9253f2f5
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-04-20 15:03:43 -0400

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (!(typeof num1=== 'number') || !(typeof num2 === 'number')){
+        return "ERROR";
+    }
+    if (Math.sign(num1) != 1 || Math.sign(num1) != 1 ){
+        return "ERROR"; 
+    }
+    if (num1> num2){
+        const temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+    sum = 0;
+    for (i = num1; i <= num2; i++) {
+        sum = sum + i;
+    }
+return sum;
 
 };
 
