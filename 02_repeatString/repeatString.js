0064af2aaa2528ab952325924364c64afad6b6a4
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-04-20 10:11:34 -0400

    Update repeatString.js
    Allow zero, but throw error message for negatives

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
 const repeatString = function(myString, num) {
+    if (num === 0){
+        return "";
+    }
+    else if (!(typeof num === 'number' && Math.sign(num)===1)){
+        return "ERROR";
+    }
     newString = "";
     for (i = 0; i < num; i++){
         newString = newString + myString;
