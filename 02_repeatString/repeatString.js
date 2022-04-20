3913b3f7d8b5c7c2eaecf4b56a1990c4165b899c
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-04-20 10:02:28 -0400

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(myString, num) {
+    newString = "";
+    for (i = 0; i < num; i++){
+        newString = newString + myString;
+    }
+    return newString;
 };
 
 // Do not edit below this line
