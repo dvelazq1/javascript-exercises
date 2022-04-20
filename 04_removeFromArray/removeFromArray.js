58966a620fb94c71a42a9a1c3a129230c8d796d7
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-04-20 13:19:03 -0400

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,10 @@
 // using rest parameters in ES6
-const removeFromArray = function(myArray, elementToRemove) {
-    for (var i = 0; i < myArray.length; i++){
-        if (myArray[i] === elementToRemove){
-        myArray.splice(i, 1);
-    }
-}
-    return myArray;
+const removeFromArray = function(... args) {
+    // the first parameter is the array 
+    const newArray = args[0];
+    // the following parameters are the arguments that should 
+    // be excluded from the array hence the not operator
+    return newArray.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
