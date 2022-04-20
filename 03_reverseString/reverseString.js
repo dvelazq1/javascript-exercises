15b28823111dd5d38becd856ef85bed6682a99e5
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-04-20 10:20:19 -0400

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(myString) {
+    const myArray = myString.split("");
+    const reversed = myArray.reverse();
+    const joined = reversed.join("");
+    return joined;
 };
 
 // Do not edit below this line
