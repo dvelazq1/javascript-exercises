1f713f5a558c102d28fa8a0da1308783557a992f
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-04-20 12:51:21 -0400

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+// using rest parameters in ES6
+const removeFromArray = function(myArray, elementToRemove) {
+    for (var i = 0; i < myArray.length; i++){
+        if (myArray[i] === elementToRemove){
+        myArray.splice(i, 1);
+    }
+}
+    return myArray;
 };
 
 // Do not edit below this line
