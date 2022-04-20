23cf965b0bbb01a672826bfe53062d992b1fe4fe
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-04-20 16:02:59 -0400

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitTemp) {
+  var cTemp = ((fahrenheitTemp-32)*(5/9));
+  return Math.round(cTemp*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusTemp) {
+  fTemp = celsiusTemp*(9/5) + 32;
+  return Math.round(fTemp*10)/10;
 };
 
 // Do not edit below this line
