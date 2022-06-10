25aa500b01715fc7d029610e621623aed6eb9a76
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-06-10 19:53:16 -0400

    completed all calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+	for (let i = 0; i <= (numbers.length-1); i++){
+    sum += numbers[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
 
+const multiply = function(numbers) {
+  let product = 1;
+	for (let i = 0; i <= (numbers.length-1); i++){
+    product *= numbers[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let num =1;
+  if (a === 0) {
+    return 1;
+  }
+  else if (a ===1) {
+    return 1;
+  }
+  else {
+
+  for(let i =a; i >= 1; i--){
+    num *= i;
+  }
+  return num;
+}
 };
 
 // Do not edit below this line
