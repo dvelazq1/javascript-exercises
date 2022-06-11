d370eb5a7ad4d9fc245983d7972b35f9841bab29
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-06-11 06:54:28 -0400

    complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.replaceAll(" ", "");
+    const regex = /[$-/:-?{-~!"^_`\[\]]/g
+    text = text.replaceAll(regex, "");
+    text = text.toLowerCase();
+    originalText = text;
+    originalText = originalText.toLowerCase();
+    text = text.split("").reverse().join("");
+    if (text === originalText){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
