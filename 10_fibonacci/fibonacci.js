69e48a464968a886159135b3927fdeecd02147a5
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-06-11 07:31:28 -0400

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    }
+    if (typeof(num) == 'string'){
+        parseInt(num);
+    }
+    myArr = [1, 1];
+    sum = 0;
+    for(let i = 2; i <= num; i++){
+        sum = myArr[i-2] + myArr[i-1];
+        myArr.push(sum);
+    }
+    return myArr[num-1];
 };
 
 // Do not edit below this line
